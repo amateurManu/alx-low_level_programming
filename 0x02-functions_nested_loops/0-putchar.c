56a5361fd5598ea7fa14268f716757070043cb20
amateurManu <oderoemmanuel6@gmail.com>
2023-10-11 11:06:39 -0700

    added program that prints out _putchar without using printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char word[] = "_putchar";
+
+	char p;
+
+	for (p = 0; p <= 7; p++)
+		_putchar(word[p]);
+		_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory covers the basics when working with functions and nested loops. The commit messages describe what the command in the file does
