d617e7965e79dcdff16a6dd42c13dfcaa1ba1fce
amateurManu <oderoemmanuel6@gmail.com>
2023-10-12 19:36:55 -0700

    added description for the print_alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * print_alphabet prints the alphabet in lowercase followed by a new line
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lower case followed by a new line
  * Return: Void
  */
 
