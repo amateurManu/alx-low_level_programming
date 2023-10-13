1af746511c2ccdd0eeca3d02e2db95466d98da61
amateurManu <oderoemmanuel6@gmail.com>
2023-10-13 01:22:35 -0700

    added program that returns the absolute value of input number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Check description
+ * @n: An input number
+ * Description: gives the absolute value of input number
+ * Return: Absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (r * -1);
+}
