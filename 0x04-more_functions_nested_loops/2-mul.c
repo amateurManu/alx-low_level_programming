3e29c5bf58aed6919763b7071bbfbc250aa38c24
amateurManu <oderoemmanuel6@gmail.com>
2023-10-13 09:24:52 -0700

    added code that multiplies two numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _mul - Check description
+ * mul - Check description
  * @a: First number to be multiplied
  * @b: Second number to be multiplied
  * Description: Function that multiplies two inputs
  * Return: Result of a multiplied by b
  */
 
-int _mul(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
