c9153acc39f8b0b975e09f7682df30fb28a6f29a
amateurManu <oderoemmanuel6@gmail.com>
2023-10-16 15:55:03 -0700

    added function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string followed by a new line
+ * @str: input string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	_putchar(str);
+	_putchar('\n');
+}
