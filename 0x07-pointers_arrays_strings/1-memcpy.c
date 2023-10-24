ead073c5139907dace7d44ca8e35efb119b320ea
amateurManu <oderoemmanuel6@gmail.com>
2023-10-23 18:17:49 -0700

    added program that copies bytes from one memory area to the other

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: destination for copied
+ * @src: to be copied
+ * @n: bytes to be copied
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	size_t = a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	
+	return (dest);
+}
