e5bd78cff003c3c944c7e588b1ff474faba53a24
amateurManu <oderoemmanuel6@gmail.com>
2023-10-25 21:47:23 -0700

    edited code that prints string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	_putchar(*s);
-	*s--;
-
 	if (*s != '\0')
-		_print_rev_recursion(*s);
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
 	else
 		_putchar('\n');
 }
