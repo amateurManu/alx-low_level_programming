4146ca269a1876bbea90edc224abefc89b075def
amateurManu <oderoemmanuel6@gmail.com>
2023-10-31 16:53:21 -0700

    edited code to allocate required memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,10 +18,10 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s2 = "";
 
-	for (a = 0; a != '\0'; a++)
+	for (a = 0; s1[a] != '\0'; a++)
 		;
 
-	for (b = 0; b != '\0'; b++)
+	for (b = 0; s2[b] != '\0'; b++)
 		;
 
 	new = malloc((a + b + 1) * sizeof(char));
