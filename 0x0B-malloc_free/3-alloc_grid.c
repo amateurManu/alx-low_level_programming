189d5232cc83de9efc3c4fee017e7035376d40bc
amateurManu <oderoemmanuel6@gmail.com>
2023-10-31 20:00:24 -0700

    first iteration

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * alloc_grid - creates a two dimensional array of integers
+ * @width: grid dimension
+ * @height: grid dimension
+ * Return: a pointer to a 2 dimensional array of integers
+ */
+
+int **grid_alloc(int width, int height)
+{
+	int **arr;
+	int a, b;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		arr[a] = malloc(width * sizeof(int));
+		if (arr[a] == NULL)
+		{
+			for (a--; a >= 0; a--)
+				free(arr[a]);
+			free(arr);
+			return (NULL);
+		}
+	}
+
+	for (a = 0; a < height; a++)
+		for (b = 0; b <width; b++)
+			arr[a][b] = 0;
+
+	return (arr);
+}
