82b10e6b208aaed489ee3da16a1c0a8ac7b16597
amateurManu <oderoemmanuel6@gmail.com>
2023-11-06 11:33:26 -0800

    defined struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - properties for a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's keeper
+ *
+ * Description: the declarations describe a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_ */
