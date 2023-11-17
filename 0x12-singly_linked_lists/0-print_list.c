3e8acc8ef56059c7034a53a8f3050d41c96752e8
amateurManu <oderoemmanuel6@gmail.com>
2023-11-16 18:20:41 -0800

    edited code to print a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,16 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes;
 
-	for (nodes = 0; h != NULL; nodes++)
+	nodes = 0;
+
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if ((*h).str == NULL)
+			printf("[0] (nil)");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", (*h).len, (*h).str);
+		h = (*h).next;
+		nodes++;
 	}
 	return (nodes);
 }
