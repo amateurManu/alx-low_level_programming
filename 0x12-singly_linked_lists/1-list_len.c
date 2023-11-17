7bb135483b0cf81c84f85d4dee201339bd55882f
amateurManu <oderoemmanuel6@gmail.com>
2023-11-16 18:53:35 -0800

    added function that returns number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in list
+ * @h: singly linked list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_y *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		h = (*h).next;
+		nodes++;
+	}
+	return (nodes);
+}
