d764cfb4c02e047bdc2968b1c253dfda72c68043
amateurManu <oderoemmanuel6@gmail.com>
2023-11-17 18:36:00 -0800

    added function that prints message before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * starter - is executed before main function
+ *
+ * Return: no value
+ */
+
+void __attribute__ ((constructor)) starter()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("I bore my house upon my back!\n");
+}
