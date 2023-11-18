4ccd21194b1b7b14ab4dd62ed4fbc2dc78f5ddd4
amateurManu <oderoemmanuel6@gmail.com>
2023-11-17 18:38:32 -0800

    edited code that prints message before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
 
 void __attribute__ ((constructor)) starter()
 {
-	printf("You're beat! and yet, you must allow,");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
