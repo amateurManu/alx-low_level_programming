efb7f73ac7d71dc9ec1468ba28ebb626ff5bac89
amateurManu <oderoemmanuel6@gmail.com>
2023-11-26 22:19:17 -0800

    edited code that converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,11 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int count, base;
-	unsigned int un_int = 0;
+	unsigned int un_int;
 
-	if (b == NULL)
+	un_int = 0;
+
+	if (!b)
 		return (0);
 
 	for (count = 0; b[count] != '\0'; count++)
@@ -20,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (count--, base = 1; count >= 0; count--, base *= 2)
 	{
-		if (b[count] != '0' && b[count] != '1')
+		if (b[count] != '1' && b[count] != '0')
 			return (0);
 		else if (b[count] & 1)
 			un_int += base;
