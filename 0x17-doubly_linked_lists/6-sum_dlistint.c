90c8b4029d18bc85ccb492a34c930f47e786f698
amateurManu <oderoemmanuel6@gmail.com>
2024-01-11 17:48:57 -0800

    added function that adds data of list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums list data
+ * @head: list head
+ *
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+
+	return (sum);
+}
